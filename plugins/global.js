49576114ceebf279e325465ccc8abd22392c570e
dtpniel <dtpozner@gmail.com>
2021-01-03 13:04:41 +0200

    small change

--- plugins/global.js
+++ plugins/global.js
@@ -2,6 +2,7 @@ export default ({ app }, inject) => {
     inject('global', {
         facebookKey: "836797929683024",
         googleKey: "194392922882-jkdlkc4n44036jh97j2foatcaidh68of.apps.googleusercontent.com",
+        network:"localhost"
     }
     )
 }
